28101289c9b9a0057e72d4cebbfdbc924f169a27
Sibgatullah <sibgatullah881@gmail.com>
2025-04-08 18:02:00 +0600

    Phitron

--- Semester-01/Module-02/2-7/if_else.c
+++ Semester-01/Module-02/2-7/if_else.c
@@ -5,9 +5,12 @@ int main(){
     scanf("%d", &tk);
     if (tk >= 100){
         printf("Let's eat burger\n");
+    } 
+    else if(tk >= 50 && tk< 100){
+        printf("let's eat fuchka\n");
     }
     else{
-        printf("Not gonna eat burger\n");
+        printf("Not gonna eat \n");
     }
 
     return 0;
