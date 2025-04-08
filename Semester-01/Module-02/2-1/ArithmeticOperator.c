9cc9e520bf7aac2f808073b76ad0b3ad13e131ac
Sibgatullah <sibgatullah881@gmail.com>
2025-04-08 17:18:11 +0600

    Phitron

--- Semester-01/Module-02/2-1/ArithmeticOperator.c
+++ Semester-01/Module-02/2-1/ArithmeticOperator.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 
 int main(){
-    int  b , sum, sub, mul ;
+    int  b,c , sum, sub, mul,mod ;
     float a, div;
-    scanf("%f %d",&a,&b);
+    scanf("%f %d %d",&a,&b,&c);
     sum = a+b;
     sub = a-b;
     mul = a*b;
     div = a/b;
+    mod = b%c;
     printf("Summation is %d\n",sum);
     printf("Subtraction is %d\n",sub);
     printf("Multiplication is %d\n",mul);
     printf("Division is %.2f\n",div);
+    printf("Modulous is %d\n" ,mod);
 
     return 0;
 }
