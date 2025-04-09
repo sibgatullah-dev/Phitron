739adec025c97ac24071c71724642410379a6916
Sibgatullah <sibgatullah881@gmail.com>
2025-04-09 22:49:20 +0600

    Phitron

---
+++ Semester-01/Module-03/3-4/more-forLoop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=20; i= i+2){
+        printf("%d\n",i);
+    }
+    for (int i = 4; i <=40; i = i+4)
+    {
+        printf("%d\n",i);
+    }
+    for (int i = 2; i <=64; i=i*2)
+    {
+        printf("%d\n",i);
+    }
+    
+    
+    return 0;
+}
