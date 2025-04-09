c4bdfe7f470e9a711585426121a91acd2f167fa5
Sibgatullah <sibgatullah881@gmail.com>
2025-04-09 22:29:16 +0600

    Phitron

--- Semester-01/Module-01/1-10/input.c
+++ Semester-01/Module-01/1-10/input.c
@@ -2,10 +2,11 @@
 
 int main(){
     int a;
+    long long int b;
     float f;
     char c;
-    scanf("%d %f %c",&a,&f,&c);// The sign & is used to get in a variable and make change in the variable value
+    scanf("%d %lld %f %c",&a,&b,&f,&c);// The sign & is used to get in a variable and make change in the variable value
     
-    printf("%d %.2f %c", a, f, c);
+    printf("%d %lld %.2f %c", a,b, f, c);
     return 0;
 }
--- Semester-01/Practice/m1_m2/Problem01.c
+++ Semester-01/Practice/m1_m2/Problem01.c
@@ -1,4 +1,4 @@
-//Write a c program that takes a number as input and chesks wether the number is positive or negative.
+//Write a c program that takes a number as input and chesks wether the number is positive or negative .
 
 #include<stdio.h>
 int main(){
