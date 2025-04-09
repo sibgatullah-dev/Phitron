bc10455853908fe3826025963ba2ae93a6d68cf5
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 04:05:59 +0600

    Phitron

---
+++ Semester-01/Module-03/3-15 while loop/whiteLoop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i = 0;//Initilization
+    while (i<=10)//End condition
+    {
+        printf("%d\n",i);//code
+        i++;//Increment or Decrement
+    }
+    
+    return 0;
+}
