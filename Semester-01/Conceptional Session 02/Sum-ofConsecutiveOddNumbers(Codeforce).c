58ea1e37a3ec18cb29f614e8a0a66095164b2444
Sibgatullah <sibgatullah881@gmail.com>
2025-04-11 01:56:03 +0600

    Phitron

--- Semester-01/Conceptional Session 02/Sum-ofConsecutiveOddNumbers(Codeforce).c
+++ Semester-01/Conceptional Session 02/Sum-ofConsecutiveOddNumbers(Codeforce).c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 int main(){
-    int x,y,sum;
+    int x,y,T;
+    scanf("%d %d %d",&T,&x,&y);
+   for (int tc = 1; tc <= T; tc++)
+   {
     scanf("%d %d",&x,&y);
-    sum = 0;
-   
+
     if (x>y){
         int z = x;
         x = y;
         y = z;
-    }    
+    }   
+
+    int sum = 0;
     for (int i = x+1; i < y; i++)
     {
         if(i%2!=0){
             sum = sum+i;
         }
     }
-    printf("%d",sum);
+    printf("%d\n",sum);
+}
+   
     
     return 0;
 }
