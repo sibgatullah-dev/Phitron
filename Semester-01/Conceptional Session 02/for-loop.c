f98a895b6d897ebbfb5ef730257b0a7feb9fd467
Sibgatullah <sibgatullah881@gmail.com>
2025-04-10 23:31:57 +0600

    Phitron

---
+++ Semester-01/Conceptional Session 02/for-loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int counter ;
+    for (counter = 1; counter <= 5; counter++)
+    {
+       
+    }
+    printf("%d",counter);
+    return 0;
+}
---
+++ Semester-01/Module-03 For-loop While-loo Do-while-loop Break Continue/Practice/Problem-01.c
@@ -0,0 +1,10 @@
+//write a c program to print 8's time table to 200
+#include<stdio.h>
+int main(){
+    for (int i = 0; i <= 200; i=i+8)
+    {
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
