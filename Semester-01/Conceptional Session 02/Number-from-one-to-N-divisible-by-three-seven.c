6502cfae2f99499ff98eaf46591f2ff47fff71cd
Sibgatullah <sibgatullah881@gmail.com>
2025-04-11 00:37:20 +0600

    Phitron

--- Semester-01/Conceptional Session 02/Number-from-one-to-N-divisible-by-three-seven.c
+++ Semester-01/Conceptional Session 02/Number-from-one-to-N-divisible-by-three-seven.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 
 int main() {
-    int N;
+    int N ,count;
     scanf("%d", &N);
-
+    count = 0;
     for (int num = 1; num <= N; num++) {
         if (num % 3 == 0 && num % 7 == 0) {
             printf("%d\n", num);
+            count++;
         }
     }
+    printf("Total values are %d",count);
 
     return 0;
 }
