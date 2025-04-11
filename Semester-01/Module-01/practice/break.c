0cb9e0a0434399381c8fd08c7a499d7bf4af124c
Sibgatullah <sibgatullah881@gmail.com>
2025-04-12 01:53:00 +0600

    Phitron

---
+++ Semester-01/Module-01/practice/break.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(i==6){
+            break;
+        }
+        printf("%d",i);// it will print only 5 cause when i =  the loop will get break and 6 won't get print!!
+    }
+    return 0;
+}
