ccbcfbf9ec57917085e9c8e563ba265f18999d6e
Sibgatullah <sibgatullah881@gmail.com>
2025-04-15 19:42:49 +0600

    Phitron

---
+++ Semester-01/Module-06/6-2/practice 2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x = 10;
+    ++x;//increments by 1
+    printf("%d",++x);//again increments by on and becomes 12 and prints 
+    return 0;
+}
--- Semester-01/Module-06/6-2/practice.c
+++ Semester-01/Module-06/6-2/practice.c
@@ -4,6 +4,7 @@ int main(){
     x = 10;
     y = x++;//y = 10 then x becomes 11
     z = ++y;//y = 10 then y becomes 11 and z becomes 11
-    printf("%d", z++);
+    printf("%d\t", z++);// z became 12 but got printed before the increment so even if z is now 12 it shows 11
+    printf("%d",z);
     return 0;
 }
