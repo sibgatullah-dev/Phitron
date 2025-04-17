36337932312d807e40c4950f6c39394391314d71
Sibgatullah <sibgatullah881@gmail.com>
2025-04-18 04:34:11 +0600

    phitron

---
+++ Semester-01/Module-07/array_initialization.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a[5] = {0};//array diclared and initialized
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d  ",a[i]);
+    }
+    
+    return 0;
+}
