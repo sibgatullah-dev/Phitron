b07753be4a6480b5f3bbe1d6cd6204328db03a9e
Sibgatullah <sibgatullah881@gmail.com>
2025-04-26 05:33:38 +0600

    Phitron

---
+++ Semester-01/Module-11/strcmp.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    char a[101],b[101];
+    scanf("%s %s",&a,&b);
+
+    int val = strcmp(a,b);
+
+    if (val < 0)
+    {
+        printf("A is small");
+    }
+    else if (val > 0)
+    {
+        printf("B is small ");
+    }
+    else if (val == 0)
+    {
+        printf("Equal");
+    }
+    
+    
+    return 0;
+}
