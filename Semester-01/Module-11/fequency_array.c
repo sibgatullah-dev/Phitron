589740d345892368fc15c106cef7701722ca0324
Sibgatullah <sibgatullah881@gmail.com>
2025-05-07 22:52:28 +0600

    pattern

---
+++ Semester-01/Module-11/fequency_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int freq[9]={0};
+    for (int i = 0; i <= 9; i++)
+    {
+       
+        
+
+    }
+    
+    
+    return 0;
+}
