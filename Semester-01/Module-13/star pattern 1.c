268c2f911070394074bc823843581c46a0b252fd
Sibgatullah <sibgatullah881@gmail.com>
2025-05-07 22:57:13 +0600

    Pattern 1

--- Semester-01/Module-13/star pattern 1.c
+++ Semester-01/Module-13/star pattern 1.c
@@ -10,6 +10,14 @@ int main(){
       }
       printf("\n"); 
     }
+    for (int i = n-1; i >= 1; i--)
+    {
+      for (int j = i; j >= 1; j--)
+      {
+        printf("*");
+      }
+      printf("\n"); 
+    }
     
     return 0;
 }
