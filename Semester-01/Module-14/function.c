23fb03c4f6248b36cb2cb1cd8cfd7f0e606505dc
Sibgatullah <sibgatullah881@gmail.com>
2025-05-09 15:27:51 +0600

    module 14

---
+++ Semester-01/Module-14/function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int sum(int a,int b){
+    int add= a+b;
+    return add;
+}
+int sub(int a,int b){
+    int minus = a-b;
+    return minus;
+}
+
+int main(){
+    int a,b;scanf("%d %d",&a,&b);
+    int ans = sum(a,b);
+    int ans2 = sub(a,b);
+    printf("%d %d",ans,ans2); 
+    return 0;
+}
