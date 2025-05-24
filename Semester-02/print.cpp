36fbb2f3eceaae5c79e599f11121d7358ae12eeb
Sibgatullah <sibgatullah881@gmail.com>
2025-05-25 01:08:47 +0600

    CPP

--- Semester-02/print.cpp
+++ Semester-02/print.cpp
@@ -4,6 +4,7 @@ int main(){
     char c = 'A';
     double d = 34.25;
     //As we don't have any spacific function to print so we use the std::cout command , the cout command is located in std name space . the << sign says what we whant form the cout command.
-    std::cout<<x<<std::endl<<c<<std::endl<<d;// the endl command prints a new line std::endl
+    std::cout<<x<<std::endl<<c<<std::endl<<d<<std::endl;// the endl command prints a new line std::endl
+    std::cout<<"My favourite number is "<<x<<std::endl;
     return 0;
 }
