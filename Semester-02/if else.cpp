2fd7cd23e72ed49313481716fbb66ea60015eac6
Sibgatullah <sibgatullah881@gmail.com>
2025-05-25 22:33:41 +0600

    CPP

---
+++ Semester-02/if else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;cin>>x;
+    if (x%2==0)
+    {
+        cout<<x<<" is Even"<<endl;
+    }else{
+        cout<<x<<" is Odd"<<endl;
+    }
+    
+    return 0;
+}
---
+++ Semester-02/ternary operator.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;cin>>x;
+    (x%2==0)?cout<<x<<" is Even"<<endl:cout<<x<<" is Odd";
+    return 0;
+}
