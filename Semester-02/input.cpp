e536704de07c879d5f800c209ac5cbeb115d8ca4
Sibgatullah <sibgatullah881@gmail.com>
2025-05-25 21:10:38 +0600

    CPP

---
+++ Semester-02/input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    char c;
+    double d;
+    int y ;
+    cin>>y;
+    cin>>x>>c>>d;
+    cout<<x<<endl<<c<<endl<<d<<endl;
+    int ascii = c;
+    cout<<ascii<<endl;
+    cout<<(int)c<<endl;
+    cout<<(char)y<<endl;
+    return 0;
+}
