19cca950b3716ba86b9eadd006c6c3755ffd26b3
Sibgatullah <sibgatullah881@gmail.com>
2025-05-25 22:21:59 +0600

    CPP

---
+++ Semester-02/EOF.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    while (cin>>x)
+    {
+        cout<<x<<endl;
+    }
+    
+    return 0;
+}
+
+//As long as we get inputs of x we will print . this is EOF
---
+++ Semester-02/setprecision.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){ 
+    double d = 23.45678;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    return 0;
+}
