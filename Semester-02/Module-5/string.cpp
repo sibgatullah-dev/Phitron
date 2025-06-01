37c376b0e49a77792ff16bb9f9211052d3f038db
Sibgatullah <sibgatullah881@gmail.com>
2025-06-01 19:10:36 +0600

    Moudule 5

---
+++ Semester-02/Module-5/string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b;
+    a="Bangladesh";
+    b="Bangladesh";
+    if(a == b){
+        cout<<"Same string"<<endl;
+    }else{
+        cout<<"Not Same string"<<endl;
+    }
+    return 0;
+}
