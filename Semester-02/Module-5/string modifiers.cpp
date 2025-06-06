8cd9b7b3bc2a1179d605eee0d32700ac4fe733f4
Sibgatullah <sibgatullah881@gmail.com>
2025-06-06 21:54:31 +0600

    Module 5

--- Semester-02/Module-5/string modifiers.cpp
+++ Semester-02/Module-5/string modifiers.cpp
@@ -19,5 +19,22 @@ int main(){
 
     s.pop_back();
     cout<<s<<endl;
+
+    string s1 = "Hi";
+    string s3 = "Hello";
+    s3.assign(s1);
+    cout<<s3<<endl;
+
+    string b = "Hello World";
+    b.erase(6,3);//first parameter indicated the index and second number in parameter indicates how many character we want to delete 
+    cout<<b<<endl;
+
+    string r = "Hello world";
+    r.replace(6,5,"Bangladesh");// first number in parameter indicates the index second number indicates how many characters should be removed and the string indicates which characters you wnat to put as replacement 
+    cout<<r<<endl;
+
+    r.insert(5," Sibgatullah");
+    cout<<r<<endl;
+
     return 0; 
 }
