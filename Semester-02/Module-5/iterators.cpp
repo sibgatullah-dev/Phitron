247aabba09fca54a6a36f642d613ef72af298124
Sibgatullah <sibgatullah881@gmail.com>
2025-06-12 15:04:48 +0600

    module 5

--- Semester-02/Module-5/iterators.cpp
+++ Semester-02/Module-5/iterators.cpp
@@ -4,5 +4,18 @@ int main(){
     string s = "Hello World";
     cout<<*s.begin()<<endl;
     cout<<*(s.end()-1)<<endl;
+
+    for (string::iterator it = s.begin(); it < s.end(); it++)
+    {
+        cout<<*it<<" ";
+    }
+cout<<endl;
+    string a = "Bangladesh";\
+    for (auto it = a.begin(); it < a.end(); it++)
+    {
+        cout<<*it<<endl;
+    }
+    
+    
     return 0;
 }
