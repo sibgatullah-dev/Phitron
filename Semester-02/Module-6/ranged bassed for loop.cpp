2e5b920492cf80f3a263cc697943571e11057bab
Sibgatullah <sibgatullah881@gmail.com>
2025-06-14 13:58:07 +0600

    Module 6

---
+++ Semester-02/Module-6/ranged bassed for loop.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    for(char c : s){ // this loop works on element not with index like regular for loop
+        cout<<c<<endl;
+    }
+    return 0;
+}
