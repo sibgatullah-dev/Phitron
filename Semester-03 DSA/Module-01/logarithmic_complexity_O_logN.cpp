351a363d10ca9f293f90e6e82f27523fc0b5cc6e
Sibgatullah <sibgatullah881@gmail.com>
2025-06-23 03:59:47 +0600

    DSA

--- Semester-03 DSA/Module-01/logarithmic_complexity_O_logN.cpp
+++ Semester-03 DSA/Module-01/logarithmic_complexity_O_logN.cpp
@@ -1,6 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n=1000;
 
+    for (int i = 0; i < n; i*=2)//-- increasing by multiple which makes it logarithmic Complexity
+    {
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+    for (int i = n; i >= 1; i/=3)//-- decrement by division which makes it logrithmic complexity
+    {
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+    int k = 2;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<i<<" ";
+        i = i*k;//-- log N
+                    //  k
+    }
+    
+    
+    
     return 0;
 }
