45defa750835987804b165034c7b0cadac2482f3
Sibgatullah <sibgatullah881@gmail.com>
2025-06-25 18:13:58 +0600

    DSA m2

--- Semester-03 DSA/Module-02  STL Vector/vector_modifiers2.cpp
+++ Semester-03 DSA/Module-02  STL Vector/vector_modifiers2.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 int main(){
     vector<int>v={1,2,3,2,4,2,5,2};
-    replace(v.begin(),v.end(),2,50);
+    replace(v.begin(),v.end(),2,50);//-- starting ending value_to_replace value_to_be_replaced_with
     for(int i:v){
         cout<<i<<" ";
+    }
+    cout<<endl;
+    //-- find function works with iterator which means you can't find the index or work with index with it 
+    vector<int>::iterator it = find(v.begin(),v.end(),50);//-- (vector<int>::iterator = auto) this indecator can be written by auto key word 
+    if(it == v.end()){
+        cout<<"NOT FOUND";
+    }else{
+        cout<<"FOUND";
     }
     return 0;
 }
