9d5527353077b08557694f34722842acdeb2b0a1
Sibgatullah <sibgatullah881@gmail.com>
2025-06-25 21:26:54 +0600

    DSA m2

---
+++ Semester-03 DSA/Module-02  STL Vector/Vector_element_access.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v = {1,2,3,4,5};
+
+    cout<<v.front()<<endl;//-- accesses the first element of the array like v[0]
+
+    cout<<v.back()<<endl;//-- accesses the last element of the array like v[v.size()-1]
+
+    return 0;
+
+}
