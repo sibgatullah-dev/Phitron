f90bb252a3650d363bb4c49853582c0435efe810
Sibgatullah <sibgatullah881@gmail.com>
2025-06-29 20:28:05 +0600

    DSA m2

--- Semester-03 DSA/Module-03  Prefix Sum and Binary Search/Binary_search.cpp
+++ Semester-03 DSA/Module-03  Prefix Sum and Binary Search/Binary_search.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n;cin>>n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
     
+    int val;cin>>val;
+
+    int flag = 0;
+
+    int l=0,r=n-1;
+    while (l<=r)
+    {
+        int mid = (l+r)/2;
+        if (a[mid]== val)
+        {
+            flag = 1;
+            break;
+        }else if (a[mid]>val)
+        {
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+        
+    }
+
+    if (flag == 1)
+    {
+        cout<<"FOUND"<<endl;
+    }else{
+        cout<<"NOT FOUND"<<endl;
+    }
+      
     return 0;
-}
+}  
