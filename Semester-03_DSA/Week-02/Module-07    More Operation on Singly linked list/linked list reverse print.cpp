40b1f2462ccc769fcb98e9f58fb7fcb319fcf05f
Sibgatullah <sibgatullah881@gmail.com>
2025-07-10 22:48:51 +0600

    more operations in linked list

--- Semester-03_DSA/Week-02/Module-07    More Operation on Singly linked list/linked list reverse print.cpp
+++ Semester-03_DSA/Week-02/Module-07    More Operation on Singly linked list/linked list reverse print.cpp
@@ -1,6 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_in_tail(Node* &head,Node* &tail, int val){
+    Node* newnode = new Node(val);
+    if(head==NULL){// Corner case
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    Node* tmp = head;
+    while (tmp->next!=NULL)
+    {
+        tmp = tmp->next;
+    }
+    tail->next = newnode;
+    tail = newnode;
     
+}
+
+void print_linked_list(Node* head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout<< temp->val<<endl;
+        temp = temp->next;
+    }
+    
+}
+
+void print_reverse(Node* tmp){
+    if (tmp == NULL)
+    {
+        return;
+    }
+    print_reverse(tmp->next);
+    cout << tmp->val <<endl;
+    
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
+    while (true)
+    {
+        cin>>val;
+        if(val == -1){
+            break;
+        }
+        insert_in_tail(head,tail,val);
+    }
+    
+    print_reverse(head);
     return 0;
 }
