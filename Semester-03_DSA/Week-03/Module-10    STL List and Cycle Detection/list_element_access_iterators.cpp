12854d7ae3c35e23095c8ece7d6f027c661432e4
Sibgatullah <sibgatullah881@gmail.com>
2025-07-24 15:57:48 +0600

    reversing singly linked list

---
+++ Semester-03_DSA/Week-03/Module-10    STL List and Cycle Detection/list_element_access_iterators.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int> l = {20,30,40,10,60,40,90,10,80};
+    l.sort();// sorting the linked list in assending
+   
+
+    return 0;
+}
---
+++ Semester-03_DSA/Week-03/Module-10    STL List and Cycle Detection/reversing_singly_linked_list.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_in_tail(Node* &head,Node* &tail, int val){
+    Node* newnode = new Node(val);
+    if(head==NULL){// Corner case
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    Node* tmp = head;
+    while (tmp->next!=NULL)
+    {
+        tmp = tmp->next;
+    }
+    tail->next = newnode;
+    tail = newnode;
+    
+}
+
+void print_linked_list(Node* head){
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout<< temp->val<<'\t';
+        temp = temp->next;
+    }
+    
+}
+
+void reverse_liknked_list(Node* &head,Node* tmp,Node* &tail){
+    if (tmp->next == NULL)
+    {
+        head = tmp;
+        return;
+    }
+    reverse_liknked_list(head,tmp->next,tail);
+    tmp->next->next = tmp;
+    tmp->next = NULL;
+    tail = tmp;
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin>>val;
+        if(val == -1){
+            break;
+        }
+        insert_in_tail(head,tail,val);
+    }
+    
+    reverse_liknked_list(head,head,tail);
+    print_linked_list(head);
+    cout<<endl;
+    cout<<head->val<<endl;
+    cout<<tail->val<<endl;
+    return 0;
+}
