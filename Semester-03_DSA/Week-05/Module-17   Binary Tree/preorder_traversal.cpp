e04b99004091ce3b83bf163ade8ad86e13edfd22
Sibgatullah <sibgatullah881@gmail.com>
2025-08-08 03:27:28 +0600

    pre order traversal

---
+++ Semester-03_DSA/Week-05/Module-17   Binary Tree/preorder_traversal.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void preorder(Node* root){
+    if (root == NULL)
+    {
+        return;
+    }
+    cout<<root->val<<' '; // root
+    preorder(root->left); // left
+    preorder(root->right); // right
+}
+
+int main(){
+    Node* root = new Node(10);
+    Node* ra = new Node(20);
+    Node* rb = new Node(30);
+    Node* raa = new Node(40);
+    Node* rab = new Node(50);
+    Node* rba = new Node(60);
+    Node* rbb = new Node(70);
+
+    root->left = ra;
+    root->right = rb;
+
+    ra->left = raa;
+    ra->right = rab;
+
+    rb->left = rba;
+    rb->right = rbb; 
+
+    preorder(root);
+    
+    return 0;
+}
