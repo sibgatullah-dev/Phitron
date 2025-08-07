19d8004c6da547f6c755c98106be9237b1e9c790
Sibgatullah <sibgatullah881@gmail.com>
2025-08-08 04:03:36 +0600

    In Order Binary tree

---
+++ Semester-03_DSA/Week-05/Module-17   Binary Tree/inorder_traversal.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void inorder(Node* root){
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left); // left
+    cout<<root->val<<' '; // root
+    inorder(root->right); // right
+}
+
+int main(){
+    Node* root = new Node(10);
+    Node* ra = new Node(20);
+    Node* rb = new Node(30);
+    Node* raa = new Node(40);
+    Node* rab = new Node(50);
+    Node* rba = new Node(60);
+    Node* rbb = new Node(70);
+
+    root->left = ra;
+    root->right = rb;
+
+    ra->left = raa;
+    ra->right = rab;
+
+    rb->left = rba;
+    rb->right = rbb; 
+
+    inorder(root);
+    
+    return 0;
+}
